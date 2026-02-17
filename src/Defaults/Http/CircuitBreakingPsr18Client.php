c40c0ed4f26da894df233ed88eac4158d5a5f41a
Greg Riley <gohany@gmail.com>
2026-02-16 20:44:30 -0500

    Allow extensions

--- src/Defaults/Http/CircuitBreakingPsr18Client.php
+++ src/Defaults/Http/CircuitBreakingPsr18Client.php
@@ -11,7 +11,7 @@ use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-final class CircuitBreakingPsr18Client implements ClientInterface
+class CircuitBreakingPsr18Client implements ClientInterface
 {
     private ClientInterface $inner;
     private CircuitBreakerInterface $breaker;
